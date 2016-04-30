c11233d3893c4f80a259930ec849b61686b8ea40
Nguyen Van Luong <luongintel2009@gmail.com>
2016-05-01 00:00:14 +0700

    Add: message table

---
+++ database/migrations/2016_04_30_165732_create_message_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content', 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('message');
+    }
+}
