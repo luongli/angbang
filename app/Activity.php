1a942a2e1b7248cab7b89853b9123fc03494c56c
Nguyen Van Luong <luongintel2009@gmail.com>
2016-04-30 22:39:39 +0700

    Add: activity table

---
+++ app/Activity.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Activity extends Model
+{
+    //
+    protected $table = 'activity';
+
+    protected $fillable = ['act', 'time'];
+}
---
+++ database/migrations/2016_04_30_153211_create_activity_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateActivityTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activity', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('act', 300);
+            $table->time('time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('activity');
+    }
+}
