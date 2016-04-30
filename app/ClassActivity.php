34cac51ae30fa45ff0b02d8c2f8b1ffbb7aa25a6
Nguyen Van Luong <luongintel2009@gmail.com>
2016-04-30 22:37:21 +0700

    Add: ClassActivity model

--- app/ClassActivity.php
+++ app/ClassActivity.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class ClassActivity extends Model
 {
     //
+    protected $table = 'class_activity';
+
+    protected $fillable = [
+    	'act_date'
+    ];
 }
