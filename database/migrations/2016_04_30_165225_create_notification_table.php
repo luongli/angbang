7577e7fe6222b8d644e77ce67cf233b6ba17775d
Nguyen Van Luong <luongintel2009@gmail.com>
2016-04-30 23:56:27 +0700

    Add: notification table

---
+++ database/migrations/2016_04_30_165225_create_notification_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotificationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notification', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('type');
+            $table->string('description', 200);
+            $table->integer('id_action');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('notification');
+    }
+}
