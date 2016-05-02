116d99ba0966fee401394cb562357317bddad45c
Nguyen Van Luong <luongintel2009@gmail.com>
2016-05-02 21:19:52 +0700

    Add: teacher_class table

---
+++ database/migrations/2016_05_02_141706_create_teacher_class_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTeacherClassTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teacher_class', function (Blueprint $table) {
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('teacher_class');
+    }
+}
