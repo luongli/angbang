236f190e5f9c74afd352462883730f96f8c91870
Nguyen Van Luong <luongintel2009@gmail.com>
2016-05-03 10:47:37 +0700

    Fix: grammar error

--- database/migrations/2016_04_30_083707_create_children_table.php
+++ database/migrations/2016_04_30_083707_create_children_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateChildrensTable extends Migration
+class CreateChildrenTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ class CreateChildrensTable extends Migration
      */
     public function up()
     {
-        Schema::create('childrens', function (Blueprint $table) {
+        Schema::create('children', function (Blueprint $table) {
             $table->increments('id');
             $table->string('fname', 30);
             $table->string('mname', 20)->nullable();
@@ -38,6 +38,6 @@ class CreateChildrensTable extends Migration
      */
     public function down()
     {
-        Schema::drop('childrens');
+        Schema::drop('children');
     }
 }
