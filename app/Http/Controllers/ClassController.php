6571fc74521f06628f348899101ea3c58ca53522
Nguyen Van Luong <luongintel2009@gmail.com>
2016-05-21 11:05:43 +0700

    Add: required packages for ClassController

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Intervention\Image\Facades\Image;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Database\DatabaseManager;
+use File;
 use App\Http\Requests;
 
 class ClassController extends Controller
